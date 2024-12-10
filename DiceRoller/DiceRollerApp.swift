9148989ae1cea0f9e2b7453420a765b34e5d9444
Ievgen Kuznietsov <ievgen.contact@gmail.com>
2024-12-10 14:41:03 +0000

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Mac on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
